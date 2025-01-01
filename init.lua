61161e4059dccc2406751312d77246debcc6acf3
michaeljamieson <michaeljamieson10@gmail.com>
2025-01-01 18:32:18 -0500

    oops return comment to og

--- init.lua
+++ init.lua
@@ -180,7 +180,7 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vvim.keymap.set('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true })vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true })im.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
