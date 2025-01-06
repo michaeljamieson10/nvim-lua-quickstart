e0d10f4c1dc12f34a202252886e3bc5e768b742d
michaeljamieson10 <michaeljamieson10@gmail.com>
2025-01-06 10:23:20 -0500

    should be okay to use on all

--- lua/custom/plugins/invicro-json.lua
+++ lua/custom/plugins/invicro-json.lua
@@ -1,25 +1,25 @@
 local M = {}
 function M.setup()
-  -- Define highlight groups
-  vim.cmd [[
-    highlight SchemaFieldKey guifg=Yellow
-    highlight SchemaFunction guifg=Green
-    highlight SchemaSpecialString guifg=Cyan
-    highlight SchemaComment guifg=Grey
-    highlight SchemaCurlyBrace guifg=Blue
-]]
-
-  -- Add syntax matching for custom highlights
-  vim.api.nvim_exec(
-    [[
-    syntax match SchemaFieldKey /"\(model\|schema\|fields\|label\|type\|inputType\|model\|required\|validator\)":/
-    syntax match SchemaFunction /\<function\s*(.*)\s*{/
-    syntax match SchemaSpecialString /\<\(required\|visible\|validator\)\>/
-    syntax match SchemaComment /\/\/.*/
-    syntax match SchemaCurlyBrace /[{}]/
-]],
-    false
-  )
+  --   -- Define highlight groups
+  --   vim.cmd [[
+  --     highlight SchemaFieldKey guifg=Yellow
+  --     highlight SchemaFunction guifg=Green
+  --     highlight SchemaSpecialString guifg=Cyan
+  --     highlight SchemaComment guifg=Grey
+  --     highlight SchemaCurlyBrace guifg=Blue
+  -- ]]
+  --
+  --   -- Add syntax matching for custom highlights
+  --   vim.api.nvim_exec(
+  --     [[
+  --     syntax match SchemaFieldKey /"\(model\|schema\|fields\|label\|type\|inputType\|model\|required\|validator\)":/
+  --     syntax match SchemaFunction /\<function\s*(.*)\s*{/
+  --     syntax match SchemaSpecialString /\<\(required\|visible\|validator\)\>/
+  --     syntax match SchemaComment /\/\/.*/
+  --     syntax match SchemaCurlyBrace /[{}]/
+  -- ]],
+  --     false
+  --   )
 
   vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
     pattern = 'Z:/Data Management/*.json',
