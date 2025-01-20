204e3933b291a49bb3573a25f2d5560aa07ec07c
michaeljamieson <michaeljamieson10@gmail.com>
2025-01-20 13:20:12 -0500

    remove unneeded command

--- init.lua
+++ init.lua
@@ -168,7 +168,6 @@ vim.opt.scrolloff = 10
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>vv', ':vsplit<CR>', { noremap = true, silent = true })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
