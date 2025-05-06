b539cd4fe3852bb8f7ad99097e023b082f8eada7
Michael Jamieson <michaeljamieson10@gmail.com>
2025-05-06 19:48:46 -0400

    import conform

--- init.lua
+++ init.lua
@@ -1052,6 +1052,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.conform',
   require 'kickstart.plugins.mini',
   require 'kickstart.plugins.which-key',
   require 'kickstart.plugins.neo-tree',
